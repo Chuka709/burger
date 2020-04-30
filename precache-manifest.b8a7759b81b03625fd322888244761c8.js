39b683a742f12e9cbd05132b9297d98d4d2c05c7
unknown <b.chuka@gmail.com>
2020-04-30 15:31:57 +0800

    Updates

---
+++ precache-manifest.b8a7759b81b03625fd322888244761c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f6f1008a34a7712ae434cf7790fa105",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "599d22232b7dd83be3d6",
+    "url": "/burger/static/css/main.50b987e1.chunk.css"
+  },
+  {
+    "revision": "902085f170d178f20d08",
+    "url": "/burger/static/js/2.053ca080.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/burger/static/js/2.053ca080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "599d22232b7dd83be3d6",
+    "url": "/burger/static/js/main.55195216.chunk.js"
+  },
+  {
+    "revision": "ccd9bcf50708292440df",
+    "url": "/burger/static/js/runtime-main.90a13846.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
